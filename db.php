83e2de7f119cedf32d0642bb2c584b279e491cc6
YulietSompotan <161845141+YulietSompotan@users.noreply.github.com>
2024-05-20 00:38:40 +0800

    blog baru

---
+++ db.php
@@ -0,0 +1,11 @@
+php
+     <?php
+     $host = 'localhost';
+     $user = 'root';
+     $password = '';
+     $dbname = 'blog';
+
+     $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+     $pdo = new PDO($dsn, $user, $password);
+     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+     ?>

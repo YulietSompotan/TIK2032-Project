9a28980b05c5f7982685a546491aa9b22676e661
YulietSompotan <161845141+YulietSompotan@users.noreply.github.com>
2024-05-21 14:03:38 +0800

    tugasphp

---
+++ function.php
@@ -0,0 +1,15 @@
+<?php
+$db = mysqli_connect("localhost", "root", "", "portofolio");
+
+function query($query) {
+    global $db;
+
+    $result = mysqli_query($db, $query);
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+?>
